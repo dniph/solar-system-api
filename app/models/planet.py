2ae69335362b019eb33220efa17aac2b2fb4e3da
Dennif <dennif.pa@gmail.com>
2025-04-24 13:32:17 -0700

    model connected with database

--- app/models/planet.py
+++ app/models/planet.py
@@ -1,17 +1,37 @@
-class Planet:
-    def __init__(self, id, name, description, distance_from_sun):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.distance_from_sun = distance_from_sun
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)    
+    name: Mapped[str]    
+    description: Mapped[str]
+    distance_from_sun:Mapped[float]
+
+
+
+
+
+
+
+
+
+
+
+
+# class Planet:
+#     def __init__(self, id, name, description, distance_from_sun):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.distance_from_sun = distance_from_sun
     
-planets = [
-    Planet(1, "Mercury", "The smallest and closest planet to the Sun", 57.9),
-    Planet(2, "Venus", "Similar in size to Earth, but with a toxic atmosphere.", 108.2),
-    Planet(3, "Earth", "Our home planet, the only known one with life.", 149.6),
-    Planet(4, "Mars", "The Red Planet, known for its dusty, rocky surface.", 227.9),
-    Planet(5, "Jupiter", "The largest planet, a gas giant with a Great Red Spot.", 778.5),
-    Planet(6, "Saturn", "Famous for its beautiful ring system.", 1_433.5),
-    Planet(7, "Uranus", "An ice giant with a tilted axis.", 2_872.5),
-    Planet(8, "Neptune", "A distant blue planet known for its strong winds.", 4_495.1)
-]
+# planets = [
+#     Planet(1, "Mercury", "The smallest and closest planet to the Sun", 57.9),
+#     Planet(2, "Venus", "Similar in size to Earth, but with a toxic atmosphere.", 108.2),
+#     Planet(3, "Earth", "Our home planet, the only known one with life.", 149.6),
+#     Planet(4, "Mars", "The Red Planet, known for its dusty, rocky surface.", 227.9),
+#     Planet(5, "Jupiter", "The largest planet, a gas giant with a Great Red Spot.", 778.5),
+#     Planet(6, "Saturn", "Famous for its beautiful ring system.", 1433.5),
+#     Planet(7, "Uranus", "An ice giant with a tilted axis.", 2872.5),
+#     Planet(8, "Neptune", "A distant blue planet known for its strong winds.", 4495.1)
+# ]
--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,46 +1,46 @@
 from flask import abort, Blueprint, make_response
-from app.models.planet import planets
+# from app.models.planet import planets
 
 planet_bp = Blueprint("plabet_bp", __name__, url_prefix="/planets")
 
 #Validations
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except ValueError:
-        invalid_error = {"message": f"Planet {planet_id} invalid"}
-        abort(make_response(invalid_error, 400))
+# def validate_planet(planet_id):
+#     try:
+#         planet_id = int(planet_id)
+#     except ValueError:
+#         invalid_error = {"message": f"Planet {planet_id} invalid"}
+#         abort(make_response(invalid_error, 400))
     
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return planet
     
-    not_found = {"message": f"Planet {planet_id} not found"}
-    abort(make_response(not_found, 404))
+#     not_found = {"message": f"Planet {planet_id} not found"}
+#     abort(make_response(not_found, 404))
 
-#Get all planets
-@planet_bp.get("")
-def get_all_planets():
-    planets_response = []
-    for planet in planets:
-        planets_response.append(dict(
-            id = planet.id,
-            name = planet.name,
-            description = planet.description,
-            distance_from_sun = planet.distance_from_sun
+# #Get all planets
+# @planet_bp.get("")
+# def get_all_planets():
+#     planets_response = []
+#     for planet in planets:
+#         planets_response.append(dict(
+#             id = planet.id,
+#             name = planet.name,
+#             description = planet.description,
+#             distance_from_sun = planet.distance_from_sun
             
-        ))
+#         ))
     
-    return planets_response
+#     return planets_response
 
 
-#Get one planet 
-@planet_bp.get("/<planet_id>")
-def get_one_planet(planet_id):
-    planet = validate_planet(planet_id)
+# #Get one planet 
+# @planet_bp.get("/<planet_id>")
+# def get_one_planet(planet_id):
+#     planet = validate_planet(planet_id)
 
-    return dict(id = planet.id,
-        name = planet.name,
-        description = planet.description,
-        distance_from_sun = planet.distance_from_sun
-        )
+#     return dict(id = planet.id,
+#         name = planet.name,
+#         description = planet.description,
+#         distance_from_sun = planet.distance_from_sun
+#         )
